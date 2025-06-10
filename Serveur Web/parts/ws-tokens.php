0d1b445d5704ff02310dd6992e5b8c8938264104
Legobis <clovis.mondon@gmail.com>
2025-06-10 09:39:16 +0200

    Ajout clé

--- Serveur Web/parts/ws-tokens.php
+++ Serveur Web/parts/ws-tokens.php
@@ -1,7 +1,7 @@
 <?php
 // Crée un token basé sur des infos de session + sel secret
 const TOKENS_DIR = "C:\\Windows\\Temp\\OPCUA25_tokens";
-$secret = '....';
+$secret = 'B9FDCF7F7FE33F9FF2D28B1E9B2F7';
 $timestamp = time();
 $token = hash_hmac('sha256', $_SESSION['username'] . '|' . session_id() . '|' . $timestamp, $secret);
 
